f977eecddb9bae0e3184e1c5c092f88b9bbc6b3c
vahid nourhani <vnourhani1986@gmail.com>
2020-12-10 17:18:24 +0330

    websocket impl

--- src/main/scala/WebsocketHandler.scala
+++
@@ -1,8 +0,0 @@
-import org.http4s._
-import cats.implicits._
-import cats.effect._
-
-object Chatroom extends IOApp {
-  def run(args: List[String]): IO[ExitCode] =
-    IO { println(args) } >> IO.pure(ExitCode.Success)
-}
