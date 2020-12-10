1be7c7aaaed5074635364b73e08c36cac0a46405
vahid nourhani <vnourhani1986@gmail.com>
2020-12-10 19:53:50 +0330

    add topic factory to differ chatrooms

--- src/main/scala/Chatroom.scala
+++ src/main/scala/Chatroom.scala
@@ -1,6 +1,9 @@
 import org.http4s._
 import cats.implicits._
+import cats.syntax.all._
 import cats.effect._
+import cats.effect.concurrent.Ref
+
 import org.http4s.websocket.WebSocketFrame
 
 import fs2._
@@ -10,11 +13,10 @@ object Chatroom extends IOApp {
   def run(args: List[String]): IO[ExitCode] = {
 
     import scala.concurrent.ExecutionContext.Implicits.global
-
-    (for {
-      topic <- Stream.eval(Topic[IO, String]("welcome to chatroom"))
-      webSocketHandler <- Stream.eval(WebSocketHandler[IO](topic))
-      exitCode <- webSocketHandler.server(9001)
+  
+    (for {                  
+      webSocketHandler <- Stream.eval(WebSocketHandler[IO])
+      exitCode <- webSocketHandler.server(9002)
     } yield exitCode).compile.last.map(_.get)
 
   }
--- src/main/scala/WebSocketHandler.scala
+++ src/main/scala/WebSocketHandler.scala
@@ -7,19 +7,49 @@ import org.http4s.websocket.WebSocketFrame.Text
 
 import cats.effect._
 import cats.syntax.all._
+import cats.effect.concurrent.Ref
 
 import fs2._
 import fs2.concurrent.Topic
 import org.http4s.server.blaze.BlazeServerBuilder
 import scala.concurrent.ExecutionContext
 
+trait TopicFactory[F[_], T] {
+  def get(key: String): F[Topic[F, T]]
+}
+
+class StringTopicFactory[F[_]: Sync: Concurrent](
+    topics: Ref[F, Map[String, Topic[F, String]]]
+) extends TopicFactory[F, String] {
+  def get(key: String): F[Topic[F, String]] =
+    for {
+      topicMap <- topics.get
+      topicOpt <- Sync[F].delay(topicMap.get(key))      
+      topic <- topicOpt match {
+        case None =>
+          for {
+            t <- Topic[F, String](s"welcome to $key")            
+            _ <- topics.modify(ts => (ts.+(key -> t), ts))
+          } yield t
+        case Some(value) => Sync[F].delay(value)
+      }
+    } yield topic
+}
+
+object TopicFactory {
+  def apply[F[_]: Sync: Concurrent]: F[TopicFactory[F, String]] =
+    for {
+      topics <- Ref.of[F, Map[String, Topic[F, String]]](Map.empty)
+      factory <- Sync[F].delay(new StringTopicFactory[F](topics))
+    } yield factory
+}
+
 trait WebSocketHandler[F[_]] extends Http4sDsl[F] {
   def routes(maxQueued: Int): HttpRoutes[F]
   def server(port: Int): Stream[F, ExitCode]
 }
-
 class WebSocketHandlerImpl[F[_]: Sync: ConcurrentEffect: Timer](
-    topic: Topic[F, String]
+    topicFactory: TopicFactory[F, String]
 )(implicit
     ec: ExecutionContext
 ) extends WebSocketHandler[F] {
@@ -28,15 +58,20 @@ class WebSocketHandlerImpl[F[_]: Sync: ConcurrentEffect: Timer](
     HttpRoutes
       .of[F] { case GET -> Root / "rooms" / room / user =>
         WebSocketBuilder[F].build(
-          topic
-            .subscribe(maxQueued)
-            .flatMap(string => Stream.eval(Sync[F].delay(Text(string)))),
+          for {
+            topic <- Stream.eval(topicFactory.get(room))
+            string <- topic.subscribe(maxQueued)
+            res <- Stream.eval(Sync[F].delay(Text(string)))
+          } yield res,
           _.flatMap(webSocketFrame =>
-            Stream.eval(
-              topic.publish1(
-                s"$user in $room: ${webSocketFrame.data.toArray.map(_.toChar).mkString}"
+            for {
+              topic <- Stream.eval(topicFactory.get(room))
+              publish <- Stream.eval(
+                topic.publish1(
+                  s"$user in $room: ${webSocketFrame.data.toArray.map(_.toChar).mkString}"
+                )
               )
-            )
+            } yield publish
           )
         )
       }
@@ -50,10 +85,14 @@ class WebSocketHandlerImpl[F[_]: Sync: ConcurrentEffect: Timer](
 }
 
 object WebSocketHandler {
-  def apply[F[_]: Sync: ConcurrentEffect: Timer](
-      topic: Topic[F, String]
-  )(implicit
+  def apply[F[_]: Sync: ConcurrentEffect: Timer](implicit
       ex: ExecutionContext
   ): F[WebSocketHandler[F]] =
-    Sync[F].delay(new WebSocketHandlerImpl[F](topic))
+    for {
+      topicFactory <- TopicFactory[F]
+      webSocketHandler <- Sync[F].delay(
+        new WebSocketHandlerImpl[F](topicFactory)
+      )
+    } yield webSocketHandler
+
 }
